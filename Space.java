63d34631d01d4198acfff9bea98286930943367f
Anish47180 <89785752+Anish47180@users.noreply.github.com>
2022-03-17 13:11:49 +0530

    Space.java
    This is my first ever program with a boolean!

---
+++ Space.java
@@ -0,0 +1,10 @@
+public class SPACE {
+
+	public static void main(String[] args)  {
+		if(5 == 6) {
+			System.out.println("equal");
+		} else if(7 == 7) {
+			System.out.println("7=7");
+		}
+	}
+}
